4b15ef9afa115bdfafd84df37d4c0fba6cacb5ae
Phil Tracton <ptracton@gmail.com>
2015-09-04 17:43:56 -0700

    Initial commit of basic LED blinking

---
+++ environment.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# Set ip your environment paths
+#
+################################################################################
+
+##
+## Prefix of the GCC ARM Cross Compiler
+##
+PREFIX	= arm-none-eabi
+
+##
+## Tools we use for building
+##
+CC	    = $(PREFIX)-gcc
+LD	    = $(PREFIX)-gcc
+ASM     = $(PREFIX)-as
+OBJCOPY	= $(PREFIX)-objcopy
+OBJDUMP	= $(PREFIX)-objdump
+AR     	= $(PREFIX)-ar
+GDB	    = $(PREFIX)-gdb
+FLASH	= $(shell which st-flash)
+MKDIR   = mkdir
+RM      = rm
+
+
+##
+## This is the path to the root of your STM32F3 Cube Root Directory
+##
+STM32F3_CUBE_ROOT_DIR=/home/ptracton/src/software/STM32F3/STM32Cube_FW_F3_V1.2.0/
