1a52ff99635934799924a868923f073fb4419927
Phil Tracton <ptracton@gmail.com>
2015-09-25 09:23:12 -0700

    MDK5 Template

---
+++ STM32F3/Template/MDK5/RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'template' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STARTUP_STM32F3xx    /* Device Startup for STM32F3 */
+
+#endif /* RTE_COMPONENTS_H */
